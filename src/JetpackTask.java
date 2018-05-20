4f282b90f54757ad31281786d9e1d9da5791d817
hybridOnly <37471742+hybridOnly@users.noreply.github.com>
2018-05-20 21:58:13 +0300

    Forgot added an offer

--- src/JetpackTask.java
+++ src/JetpackTask.java
@@ -1,4 +1,5 @@
 import com.flowpowered.math.vector.Vector3d;
+import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 
 public class JetpackTask
@@ -6,6 +7,7 @@ public class JetpackTask
     public static void start(Player p)
     {
         p.setVelocity(p.getVelocity().add(new Vector3d(0, 10, 0)));
+        p.offer(Keys.IS_ELYTRA_FLYING, true);
         ParticleManager.cloud(p);
         p.clearTitle();
     }
